fd6f87e73abab3a90dafae04df4d48a40a03daf8
kuato <kuatomail@gmail.com>
2026-09-10 00:29:28 -0300

    refactor: improve enterprise detail

--- src/app/lib/enterprise-detail/enterprise-detail.component.ts
+++ src/app/lib/enterprise-detail/enterprise-detail.component.ts
@@ -6,7 +6,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./enterprise-detail.component.scss'],
 })
 export class EnterpriseDetailComponent {
-  @Input() razonSocial?: string;
-  @Input() cuit?: string;
+  @Input() razonSocial = '';
+  @Input() cuit = '';
   @Output() handlerCuit: EventEmitter<string> = new EventEmitter();
+
+  get formattedCuit(): string {
+    return this.cuit.replace(/^(\d{2})(\d{8})(\d)$/, '$1-$2-$3');
+  }
 }
