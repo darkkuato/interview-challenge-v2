2af100f7164143871e9b16c642862df99c7713e2
kuato <kuatomail@gmail.com>
2026-09-10 00:30:03 -0300

    refactor: improve enterprise modal

--- src/app/lib/modal-aside/modal-aside.component.ts
+++ src/app/lib/modal-aside/modal-aside.component.ts
@@ -1,23 +1,28 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-modal-aside',
   templateUrl: './modal-aside.component.html',
   styleUrls: ['./modal-aside.component.scss'],
 })
-export class ModalAsideComponent {
-  showModal: boolean = false;
+export class ModalAsideComponent implements OnDestroy {
+  @Input() showModal = false;
+  @Input() ariaLabel = 'Panel lateral';
 
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
   show(): void {
-    this.document.body.style.overflowY = 'none';
+    this.document.body.style.overflowY = 'hidden';
     this.showModal = true;
   }
 
   hide(): void {
     this.document.body.removeAttribute('style');
     this.showModal = false;
+  }
+
+  ngOnDestroy(): void {
+    this.document.body.removeAttribute('style');
   }
 }
