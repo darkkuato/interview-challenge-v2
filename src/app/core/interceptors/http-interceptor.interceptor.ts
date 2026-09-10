a12e2b80fb533f6f80c649f689923b4dacd56c4f
kuato <kuatomail@gmail.com>
2026-09-10 00:28:41 -0300

    feat: attach session request headers

--- src/app/core/interceptors/http-interceptor.interceptor.ts
+++ src/app/core/interceptors/http-interceptor.interceptor.ts
@@ -6,13 +6,32 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { BankingSessionService } from '@/services/banking-session.service';
 
 @Injectable()
 export class HttpInterceptorInterceptor implements HttpInterceptor {
+  constructor(private readonly session: BankingSessionService) {}
+
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const headers: Record<string, string> = {};
+    const token = this.session.token;
+    const enterprise = this.session.selectedEnterprise;
+
+    if (token && !request.url.endsWith('/auth/sign-in')) {
+      headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    if (enterprise && request.url.endsWith('/transferencias')) {
+      headers['CUIT'] = enterprise.cuit;
+    }
+
+    const authenticatedRequest = Object.keys(headers).length
+      ? request.clone({ setHeaders: headers })
+      : request;
+
+    return next.handle(authenticatedRequest);
   }
 }
